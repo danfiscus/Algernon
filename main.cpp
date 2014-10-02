afc74397b8868971c13dae3d771f54170f68759d
Jacob Bender <benderjacob44@gmail.com>
2014-10-02 15:46:45 -0400

    Started with parsing the input string into the array of strings

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 using namespace std;
 
-int main(){
-	cout<< "File for algernon-base"<<endl;
+int main() {
+	lexer lex1("hello");
+	lex1.make_array();
+	cout << lex1.return_expression() << endl;
 	return 0;
 }
