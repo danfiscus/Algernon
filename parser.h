064f53d45df8bef49626a2d5a695a207cca62c22
Jacob Bender <benderjacob44@gmail.com>
2014-10-06 19:07:24 -0400

    I made the tokens array which is an array of structures. This will allow for lexing the various parts of the expression into their various types.

--- parser.h
+++ parser.h
@@ -6,22 +6,35 @@
 using namespace std;
 
 const int array_len = 100;
-string tokens[array_len]; /*max size is 100*/
-
+string characters[array_len]; /*max size is 100*/
+struct token_template {
+	string type;
+	string value;
+};
+token_template tokens[array_len];
 class lexer {
 	int length;
 	string to_parse;
 	public:
 		lexer(string expression){to_parse=expression;}
 		void make_array();
+		void make_tokens();
 };
 
 void lexer::make_array() {
 	length=to_parse.length();
 	for (int i=0; i<length;i++){
-		tokens[i]=to_parse[i];
+		characters[i]=to_parse[i];
 	}
 };
+
+void lexer::make_tokens() {
+	for (int i=0;i<length;i++){
+		tokens[i].type="string";
+		tokens[i].value=characters[i];
+	};
+};
+
 
 class parser {
 	public:
@@ -30,6 +43,9 @@ class parser {
 
 void parser::print_string(){
 	for (int i=0;i<array_len;i++){
-		cout << tokens[i] << endl;
+		if (tokens[i].value!=""){
+			cout << tokens[i].value << endl;
+		}
 	}
 };
+
